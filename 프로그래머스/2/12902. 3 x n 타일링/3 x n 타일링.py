c432025fb8bea0052138371c5cc41449619c5f90
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2025-02-14 09:36:28 +0900

    [level 2] Title: 3 x n 타일링, Time: 1.65 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -1,5 +1,7 @@
 def solution(n):
     dp = [1] + [0]*(n//2)
+    stack = 0
     for i in range(1, n//2+1):
-        dp[i] = dp[i-1]*3
+        stack += dp[i-2]*2
+        dp[i] = dp[i-1]*3 + stack
     return dp[n//2] % 1000000007
