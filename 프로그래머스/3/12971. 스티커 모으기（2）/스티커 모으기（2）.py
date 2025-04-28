d914816f61f7037292d6dbfec4f8088168cb82c1
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2025-04-29 00:43:03 +0900

    [level 3] Title: 스티커 모으기(2), Time: 68.20 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -7,7 +7,7 @@ def solution(sticker):
     dp2 = [0, sticker[1], sticker[2]] + [0]*(n-3)
     
     for i in range(3, n):
-        dp1[i] = max(dp1[i-2]+sticker[i], dp1[i-3]+sticker[i])
-        dp2[i] = max(dp2[i-2]+sticker[i], dp2[i-3]+sticker[i])
+        dp1[i] = max(dp1[i-2], dp1[i-3]) + sticker[i]
+        dp2[i] = max(dp2[i-2], dp2[i-3]) + sticker[i]
     dp1[-1] = 0
     return max(max(dp1), max(dp2))
